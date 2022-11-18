8275bcf83ceaebcf694d094b8d04b284c18410c2
Maxi Ferreira <charca@gmail.com>
2022-11-18 16:37:15 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: netlify()
+});
